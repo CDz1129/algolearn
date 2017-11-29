f0e7e77694775ec832b6c34ce65b110300855fa4
CDz1129 <469176834@qq.com>
2017-11-29 10:22:21 +0800

    sync learn

--- src/main/java/cn/cdz/thread/ThreadTest.java
+++ src/main/java/cn/cdz/thread/ThreadTest.java
@@ -1,5 +1,6 @@
 package cn.cdz.thread;
 
+import com.sun.org.apache.xerces.internal.dom.PSVIAttrNSImpl;
 import org.junit.Test;
 
 /**
@@ -13,7 +14,7 @@ public class ThreadTest {
         @Override
         public void run() {
             System.out.println("线程："+getName()+"开始");
-            for (int i = 0; i < 10; i++) {
+            for (int i = 0; i < 4; i++) {
                 System.out.println(i+" "+getName());
                 try {
                     Thread.sleep(1000);
@@ -31,7 +32,7 @@ public class ThreadTest {
         public void run() {
             String name = Thread.currentThread().getName();
             System.out.println("thread-name:"+name+"开始");
-            for (int i = 0; i < 10; i++) {
+            for (int i = 0; i < 4; i++) {
                 System.out.println(i+" "+name);
                 try {
                     Thread.sleep(1000);
@@ -44,26 +45,26 @@ public class ThreadTest {
     }
 
     public static void main(String[] args) {
-
-    }
-
-    @Test
-    public void testJoin(){
         System.out.println(Thread.currentThread().getName()+"线程开始");
         MyThreadOne a = new MyThreadOne("a");
         MyThreadOne b = new MyThreadOne("b");
         a.start();
+        try {
+            Thread.sleep(2000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
         b.start();
         try {
-            b.join();
+            a.join();
+            System.out.println("a join执行完了~");
+
             //join 方法 如果没有传入时间时，当b线程结束之后才会执行下面的代码。
             //当传入时间时，由于在当时间过后，就会执行下面的代码
             //这说明：当使用join时，执行下面代码的条件只有，当时间过时，和调用此方法的线程结束时。
-            System.out.println("join执行完了~");
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
         System.out.println(Thread.currentThread().getName()+"线程结束");
     }
-
 }
